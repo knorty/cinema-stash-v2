08f6c8af9f83fd9290a996b6f85184b31c0fd7a8
Kyle Norton <kylenorton@Kyles-MBP.attlocal.net>
2022-02-28 23:16:19 -0600

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
+import './scss/app.scss';
+import SearchResults from './components/SearchResults';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Intro from './components/Intro';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  state = {
+    myMovies: [],
+    movies: []
+  }
+
+  requestMovie = async (movie) => {
+    console.log(movie)
+    const response = await axios.post('http://localhost:8000/search', { movie });
+    console.log(response);
+    this.setState({
+      movies: response.data.results
+    })
+  }
+
+  render() {
+    const searchResults = this.state.movies.map((movie) => {
+      const movieImage = "http://image.tmdb.org/t/p/w185/" + movie.poster_path
+      return (
+        <SearchResults
+          id={movie.id}
+          title={movie.title}
+          movieImage={movieImage}
+          release_date={movie.release_date}
+          overview={movie.overview}
+        />
+      )
+    })
+
+    return (
+      <div className="app">
+        <Header requestMovie={this.requestMovie} />
+        <Intro />
+        <div className="body">
+          {searchResults}
+        </div>
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
